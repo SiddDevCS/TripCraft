1808065e5678dc799fa6317696b3a5502d6c285b
Sidd0Hari <65342042+Siddharth11sehgal@users.noreply.github.com>
2025-03-03 19:48:11 +0100

    Initial Commit

---
+++ TripCraft/TripCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripCraftApp.swift
+//  TripCraft
+//
+//  Created by Siddharth Sehgal on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TripCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TripCraftTests/TripCraftTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripCraftTests.swift
+//  TripCraftTests
+//
+//  Created by Siddharth Sehgal on 03/03/2025.
+//
+
+import Testing
+@testable import TripCraft
+
+struct TripCraftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
